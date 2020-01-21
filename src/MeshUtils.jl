bff0dcdb45a1963e3c56128089a7eec363c190cd
FernandoConde-Pumpido <fernando.conde-pumpido.velasco@alumnos.upm.es>
2020-01-21 19:01:58 +0100

    No changes.

---
+++ src/MeshUtils.jl
@@ -0,0 +1,21 @@
+#= -----------------------------------------------------------------------------
+FILE: Mesh.jl
+AUTHOR: Fernando Conde-Pumpido Velasco
+DATE: 2020/01/21
+VERSION: v0.1
+CONTENTS:
+
+SUMMARY: Mesh objects and functions
+----------------------------------------------------------------------------- =#
+module MeshUtils
+
+mutable struct Mesh
+    type
+    points
+    elements
+    
+end # struct
+
+
+
+end  # module Mesh
